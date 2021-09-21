8396708cf6ca639e6abd96e0dac55317edde935c
Huynh Van Tai <taiprogramer@protonmail.com>
2021-09-21 12:13:22 +0700

    [format: add .editorconfig]
    - Use space for indentation.

--- src/main/java/EchoService/Client.java
+++ src/main/java/EchoService/Client.java
@@ -7,34 +7,34 @@ import java.net.Socket;
 
 /**
  * Simple client to demonstrate Echo service.
- * 
+ *
  * @author taiprogramer
  * */
 
 class Client {
     private static final int ECHO_PORT = 3000;
     public static void main(String args[]) {
-	try (Socket socket = new Socket("127.0.0.1", ECHO_PORT)) {
-	    InputStream iStream  = socket.getInputStream();
-	    OutputStream oStream = socket.getOutputStream();
-	    System.out.println("Connected");
+        try (Socket socket = new Socket("127.0.0.1", ECHO_PORT)) {
+            InputStream iStream  = socket.getInputStream();
+            OutputStream oStream = socket.getOutputStream();
+            System.out.println("Connected");
 
-	    int ch;
-	    while(true) {
-		ch = System.in.read();
-		if (ch == '\r' || ch == '\n') {
-		    continue;
-		}
-		if (ch == '.') {
-		    break;
-		}
-		oStream.write(ch);
-		System.out.println(String.format("me: %c", ch));
-		ch = iStream.read();
-		System.out.println(String.format("server: %c", ch));
-	    }
-	} catch(IOException e) {
-	    System.err.println(e);
-	}
+            int ch;
+            while(true) {
+                ch = System.in.read();
+                if (ch == '\r' || ch == '\n') {
+                    continue;
+                }
+                if (ch == '.') {
+                    break;
+                }
+                oStream.write(ch);
+                System.out.println(String.format("me: %c", ch));
+                ch = iStream.read();
+                System.out.println(String.format("server: %c", ch));
+            }
+        } catch(IOException e) {
+            System.err.println(e);
+        }
     }
 }
--- src/main/java/EchoService/Server.java
+++ src/main/java/EchoService/Server.java
@@ -13,35 +13,40 @@ import java.net.Socket;
  * */
 
 class Server {
-    private static final int ECHO_PORT = 3000; // to avoid root permission required on Linux
+    // to avoid root permission required on Linux
+    private static final int ECHO_PORT = 3000;
     public static void main(String args[]) {
-	try (ServerSocket server = new ServerSocket(ECHO_PORT)) {
-	    System.out.println("Server has been started");
-	    while(true) {
-		socketHandler(server.accept());
-	    }
-	} catch(IOException e) {
-	    System.err.println(e);
-	}
+        try (ServerSocket server = new ServerSocket(ECHO_PORT)) {
+            System.out.println("Server has been started");
+            while(true) {
+                socketHandler(server.accept());
+            }
+        } catch(IOException e) {
+            System.err.println(e);
+        }
     }
 
     private static void socketHandler(final Socket socket) {
-	Thread t = new Thread(() -> {
-	    String ip = socket.getInetAddress().toString();
-	    int port = socket.getPort();
-	    System.out.println(String.format("Got new connection"));
-	    System.out.println(String.format("Addr: %s:%d | Connected", ip, port));
-	    try {
-		InputStream iStream  = socket.getInputStream();
-		OutputStream oStream = socket.getOutputStream();
-		while(true) {
-		    int ch = iStream.read();
-		    oStream.write(ch);
-		}
-	    } catch(IOException e) {
-		System.out.println(String.format("Addr: %s:%d | Disconnected", ip, port));
-	    }
-	});
-	t.start();
+        Thread t = new Thread(() -> {
+            String ip = socket.getInetAddress().toString();
+            int port = socket.getPort();
+            System.out.println(String.format("Got new connection"));
+            System.out.println(
+                    String.format("Addr: %s:%d | Connected", ip, port));
+            try(
+                    InputStream iStream  = socket.getInputStream();
+                    OutputStream oStream = socket.getOutputStream();
+               ) {
+                while(true) {
+                    int ch = iStream.read();
+                    oStream.write(ch);
+                }
+            } catch(IOException e) {
+                System.out.println(
+                        String.format(
+                            "Addr: %s:%d | Disconnected", ip, port));
+            }
+        });
+        t.start();
     }
 }
